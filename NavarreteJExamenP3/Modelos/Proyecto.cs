96358cec59ff15306afb7d1cb228b4ef6d801c59
Jeffreyna21 <jeffrey.navarrete@udla.edu.ec>
2025-07-07 07:48:02 -0500

    modelos y vistas modelos

---
+++ NavarreteJExamenP3/Modelos/Proyecto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavarreteJExamenP3.Modelos
+{
+    class Proyecto
+    {
+
+        public int Id { get; set; }
+        public string NombreProyecto { get; set; }
+        public string Descripcion { get; set; }
+        public int Progreso { get; set; }
+        public int DuracionDias { get; set; }
+
+    }
+}
---
+++ NavarreteJExamenP3/ViewModel/RegistroViewModel.cs
@@ -0,0 +1,40 @@
+﻿using NavarreteJExamenP3.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NavarreteJExamenP3.ViewModel
+{
+    class RegistroViewModel : INotifyPropertyChanged
+    {
+        public Proyecto Proyecto { get; set; }= new Proyecto();
+        public ICommand GuardarCommand { get; }
+        public RegistroViewModel()
+        {
+            GuardarCommand = new Command(async () => await Guardar());    
+        }
+        private async Task Guardar()
+        {
+            if (Proyecto.Progreso > 50 && Proyecto.DuracionDias < 365)
+            {
+                await Shell.Current.DisplayAlert("Error", "El progreso no puede ser mayor al 50% si la duración es menor a 365 días.", "OK");
+                return;
+            }
+            await ProyectoService.Instance.AgregarProyecto(Proyecto);
+            await LogService.Instance.AppendLog(Proyecto.NombreProyecto);
+            await Shell.Current.DisplayAlert("Exito","Proyecto Guardado");
+
+            Proyecto = new Proyecto(); 
+            OnPropetyChanged(nameof(Proyecto));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropetyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+
+    }
+}

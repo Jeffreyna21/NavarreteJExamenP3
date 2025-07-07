91e647b986f639034dc9224cc628388adccf4cfd
laboratorio <laboratorio@UPOAULA10702>
2025-07-07 07:16:19 -0500

    Agregar archivos de proyecto.

---
+++ NavarreteJExamenP3/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NavarreteJExamenP3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

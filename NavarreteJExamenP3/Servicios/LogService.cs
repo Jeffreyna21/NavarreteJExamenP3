c71a074b9152f53b3383ecaf1ef37292049f4725
Jeffreyna21 <jeffrey.navarrete@udla.edu.ec>
2025-07-07 08:40:48 -0500

    servicio proyecto SQLite

---
+++ NavarreteJExamenP3/Servicios/LogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavarreteJExamenP3.Servicios
+{
+    class LogService
+    {
+        private readonly string logFilePath = Path.Combine(FileSystem.AppDataDirectory, "logs_Navarrete.txt");
+        public static LogService Instance { get; } = new LogService();
+        public async Task AppendLog(string nombreProyecto)
+        {
+            
+        }
+    }
+}
---
+++ NavarreteJExamenP3/Servicios/ProyectoService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavarreteJExamenP3.Servicios
+{
+    class ProyectoService
+    {
+        private static SQLiteAsyncConnection _database;
+        public static ProyectoService Instance { get; } = new ProyectoService();
+        private ProyectoService() { }
+        public async Task Init()
+        {
+            if (_database != null)
+                return;
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "proyectos.db3");
+            _database = new SQLiteAsyncConnection(dbPath);
+            await _database.CreateTableAsync<Proyecto>();
+        }
+        public Task<List<Proyecto>> ObtenerProyectos() =>
+            _database.Table<Proyecto>().ToListAsync();
+        public Task<int> AgregarProyecto(Proyecto p) => _database.InsertAsync(p);
+
+    }
+}

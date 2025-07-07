09d948513f03a0b1e91e598f7f4f61e86297c975
Jeffreyna21 <jeffrey.navarrete@udla.edu.ec>
2025-07-07 08:45:09 -0500

    servicio de logs

--- NavarreteJExamenP3/Servicios/LogService.cs
+++ NavarreteJExamenP3/Servicios/LogService.cs
@@ -8,11 +8,22 @@ namespace NavarreteJExamenP3.Servicios
 {
     class LogService
     {
-        private readonly string logFilePath = Path.Combine(FileSystem.AppDataDirectory, "logs_Navarrete.txt");
+        private readonly string logFilePath = Path.Combine(FileSystem.AppDataDirectory, "Logs_Navarrete.txt");
         public static LogService Instance { get; } = new LogService();
+
         public async Task AppendLog(string nombreProyecto)
         {
-            
+            string log = $"Se incluyó el registro [{nombreProyecto}] el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+            await File.AppendAllTextAsync(logFilePath, log);
+        }
+
+        public async Task<List<string>> LeerLogs()
+        {
+            if (!File.Exists(logFilePath))
+                return new List<string>();
+
+            var lineas = await File.ReadAllLinesAsync(logFilePath);
+            return lineas.ToList();
         }
     }
 }
